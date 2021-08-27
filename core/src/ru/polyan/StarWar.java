2b62f362e925d6d3b6f5cb6c137fee7afbedbf38
Alexay Polyashov <polyashov@yandex.ru>
2021-08-27 23:35:37 +0300

    добавил изображение и вывел на экран

--- core/src/ru/polyan/StarWar.java
+++ core/src/ru/polyan/StarWar.java
@@ -8,17 +8,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarWar extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture bgr;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		bgr = new Texture("background_512.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(bgr, 0, 0, 640,480);
 		batch.draw(img, 50, 50);
 		batch.end();
 	}
