a25f1581b0f2c4ae77129241e6bcacf0f47f79c2
Alexay Polyashov <polyashov@yandex.ru>
2021-09-14 23:17:57 +0300

    Разделение на спрайты

--- core/src/ru/polyan/base/BaseScreen.java
+++ core/src/ru/polyan/base/BaseScreen.java
@@ -5,16 +5,43 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import ru.polyan.math.MatrixUtils;
+import ru.polyan.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
+
+    protected Matrix4 worldToGl;
+    protected Matrix3 screenToWorld;
+
+    protected Vector2 touch;
+
+
     @Override
     public void show() {
+
+        batch = new SpriteBatch();
+        screenBounds = new Rect();
+        worldBounds = new Rect();
+        glBounds = new Rect(0, 0, 1f, 1f);
+
+        worldToGl = new Matrix4();
+        screenToWorld = new Matrix3();
+        touch = new Vector2();
+
         batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this);
+
     }
 
     @Override
@@ -24,7 +51,21 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void resize(int width, int height) {
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
 
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        resize(worldBounds);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+        System.out.println("resize worldBounds.width = " + worldBounds.getWidth() + " worldBounds.height = " + worldBounds.getHeight());
     }
 
     @Override
@@ -65,18 +106,37 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
         return false;
     }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        return false;
+    }
+
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
