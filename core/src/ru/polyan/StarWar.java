573482de8bae2d9425104284f4e97c534c9d85c8
Alexay Polyashov <polyashov@yandex.ru>
2021-09-15 22:24:36 +0300

    Звездное поле

--- core/src/ru/polyan/StarWar.java
+++ core/src/ru/polyan/StarWar.java
@@ -12,7 +12,8 @@ public class StarWar extends Game {
 
 	@Override
 	public void create () {
-		setScreen(new MenuScreen());
+		setScreen(new MenuScreen(this));
 	}
+
 
 }
--- core/src/ru/polyan/base/BaseScreen.java
+++ core/src/ru/polyan/base/BaseScreen.java
@@ -15,33 +15,31 @@ import ru.polyan.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
 
-    protected SpriteBatch batch;
-
     private Rect screenBounds;
     private Rect worldBounds;
     private Rect glBounds;
 
-    protected Matrix4 worldToGl;
-    protected Matrix3 screenToWorld;
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
 
-    protected Vector2 touch;
+    private Vector2 touch;
 
+    protected SpriteBatch batch;
 
     @Override
     public void show() {
-
-        batch = new SpriteBatch();
+        System.out.println("show");
         screenBounds = new Rect();
         worldBounds = new Rect();
         glBounds = new Rect(0, 0, 1f, 1f);
 
         worldToGl = new Matrix4();
         screenToWorld = new Matrix3();
+
         touch = new Vector2();
 
         batch = new SpriteBatch();
         Gdx.input.setInputProcessor(this);
-
     }
 
     @Override
@@ -51,6 +49,7 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void resize(int width, int height) {
+        System.out.println("resize width = " + width + " height = " + height);
         screenBounds.setSize(width, height);
         screenBounds.setLeft(0);
         screenBounds.setBottom(0);
@@ -70,73 +69,82 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void pause() {
-
+        System.out.println("pause");
     }
 
     @Override
     public void resume() {
-
+        System.out.println("resume");
     }
 
     @Override
     public void hide() {
+        System.out.println("hide");
         dispose();
     }
 
     @Override
     public void dispose() {
+        System.out.println("dispose");
         batch.dispose();
     }
 
-
     @Override
     public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
+        System.out.println("keyUp keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
+        System.out.println("keyTyped character = " + character);
         return false;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
         touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
         touchDown(touch, pointer, button);
         return false;
     }
 
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        System.out.println("touchDown touch.X = " + touch.x + " touch.Y = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
         touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
         touchUp(touch, pointer, button);
         return false;
     }
 
     public boolean touchUp(Vector2 touch, int pointer, int button) {
+        System.out.println("touchUp touch.X = " + touch.x + " touch.Y = " + touch.y);
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
         touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
         touchDragged(touch, pointer);
         return false;
     }
 
     public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchDragged touch.X = " + touch.x + " touch.Y = " + touch.y);
         return false;
     }
-
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
@@ -145,6 +153,7 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(float amountX, float amountY) {
+        System.out.println("scrolled amountX = " + amountX + " amountY = " + amountY);
         return false;
     }
 }
